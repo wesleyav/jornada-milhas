2b10ab8c1b447cc15777da00d3152799712c3edd
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-07-04 17:10:42 -0300

    Adiciona e configura dependência springdoc-openapi

--- src/main/java/com/github/wesleyav/jornadamilhas/JornadamilhasApplication.java
+++ src/main/java/com/github/wesleyav/jornadamilhas/JornadamilhasApplication.java
@@ -1,13 +1,27 @@
 package com.github.wesleyav.jornadamilhas;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class JornadamilhasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JornadamilhasApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI().info(new Info().title("Jornada Milhas").version(appVersion)
+				.description("Projeto jornada-milhas da Alura Challenge Back-End 7")
+				.termsOfService("http://swagger.io/terms/")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
