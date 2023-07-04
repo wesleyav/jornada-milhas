5fcd5399d605b56b8156493f8520110bca24115a
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-07-04 17:07:20 -0300

    Adiciona exceções personalizadas

---
+++ src/main/java/com/github/wesleyav/jornadamilhas/services/exceptions/ResourceEmptyException.java
@@ -0,0 +1,11 @@
+package com.github.wesleyav.jornadamilhas.services.exceptions;
+
+public class ResourceEmptyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceEmptyException(String message) {
+		super(message);
+	}
+
+}
