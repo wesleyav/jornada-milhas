3ffe4af14945a19bd0a2dfa0c1b093f5008a1148
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-07-31 10:46:14 -0300

    Atualiza teste para destino

--- src/test/java/com/github/wesleyav/jornadamilhas/common/DestinoConstants.java
+++ src/test/java/com/github/wesleyav/jornadamilhas/common/DestinoConstants.java
@@ -1,13 +1,10 @@
 package com.github.wesleyav.jornadamilhas.common;
 
-import java.math.BigDecimal;
-
 import com.github.wesleyav.jornadamilhas.entities.Destino;
 
 public class DestinoConstants {
 
-//	public static final Destino DESTINO = new Destino(1L, "asdf", "asdf", new BigDecimal("100.50"));
-	public static final Destino DESTINO = new Destino(1L, "asdf", "asdf", null);
-	public static final Destino DESTINO_INVALIDO = new Destino(1L, "asdf", "asdf", null);
+	public static final Destino DESTINO = new Destino(1L, "foto1", "foto2", "nome", "meta", "td", null);
+	public static final Destino DESTINO_INVALIDO = new Destino(1L, "foto1", "foto2", "nome", "meta", "td", null);
 
 }
